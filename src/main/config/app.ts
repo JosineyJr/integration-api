1339035508a9894e3a548fc315945493a223595a
Josiney Mafra <josiney.mafra@gcbinvestimentos.com>
2021-10-09 04:36:05 -0300

    refactor: 💡 use express.json()

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,10 @@
 import express, { Express } from 'express';
+import { setupRoutes } from '@/main/config/routes';
 
 export const setupApp = async (): Promise<Express> => {
   const app = express();
+  app.use(express.json());
+  setupRoutes(app);
 
   return app;
 };
