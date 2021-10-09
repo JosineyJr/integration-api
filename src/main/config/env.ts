d32fb4d28fa157419347296cd8b085704db34f19
Josiney Mafra <josiney.mafra@gcbinvestimentos.com>
2021-10-09 04:36:35 -0300

    refactor: 💡 add mongoUri env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,4 +2,5 @@ export default {
   port: process.env.APPLICATION_PORT || 3032,
   jwtSecret: process.env.JWT_SECRET || '307eb648-937a-451b-9e5f-f8a6e46b96fb',
   connectionName: process.env.CONNECTION_NAME || 'pg',
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/pipedrive-bling-integration-api',
 };
