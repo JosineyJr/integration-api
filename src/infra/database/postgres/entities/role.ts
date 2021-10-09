d0f05f0d11f44b09cf9571226aa1151ee8d508c4
Josiney Mafra <josiney.mafra@gcbinvestimentos.com>
2021-10-09 16:58:55 -0300

    feat: 🎸 add role pg entity

---
+++ src/infra/database/postgres/entities/role.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { PgUser } from './user';
+
+@Entity({ name: 'tb_roles' })
+export class PgRole {
+  @PrimaryGeneratedColumn('increment')
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column({ name: 'created_at' })
+  createdAt!: Date;
+
+  @OneToOne(() => PgUser, user => user.role)
+  user!: PgUser;
+}
