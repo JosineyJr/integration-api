c64bcdd612f99ba595af3536edc64cd68388c904
Josiney Mafra <josiney.mafra@gcbinvestimentos.com>
2021-10-08 23:02:34 -0300

    refactor: 💡 ad email property

--- src/domain/use-cases/add-user.ts
+++ src/domain/use-cases/add-user.ts
@@ -1,11 +1,12 @@
 export namespace AddUser {
   export type Params = {
     name: string;
+    email: string;
     password: string;
   };
   export type Result = boolean;
 }
 
 export interface IAddUser {
-  add: (user: AddUser.Params) => Promise<AddUser.Result>
+  add: (user: AddUser.Params) => Promise<AddUser.Result>;
 }
