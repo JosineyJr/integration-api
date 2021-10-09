00a6c46a61db1a9744bc3a2c51ebf14889e672ef
Josiney Mafra <josiney.mafra@gcbinvestimentos.com>
2021-10-09 16:58:27 -0300

    refactor: 💡 add try catch

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '../helpers';
+import { badRequest, serverError } from '../helpers';
 import { IValidator } from '../protocols';
 import { HttpResponse } from '../protocols/http';
 import { ValidationComposite } from '../validation';
@@ -18,8 +18,12 @@ export abstract class Controller {
 
   async handle(httpRequest: any): Promise<HttpResponse> {
     const error = this.validate(httpRequest);
-
     if (error !== undefined) return badRequest(error);
-    return this.perform(httpRequest);
+
+    try {
+      return await this.perform(httpRequest);
+    } catch (erro: any) {
+      return serverError(erro);
+    }
   }
 }
