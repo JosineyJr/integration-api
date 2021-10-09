391b1adbde5a70a09842bf452b6fe5c1dc34e4cc
Josiney Mafra <josiney.mafra@gcbinvestimentos.com>
2021-10-09 18:39:31 -0300

    feat: 🎸 add type validation

---
+++ src/application/validation/type.ts
@@ -0,0 +1,13 @@
+import { IncorrectFieldTypeError } from '../errors/validation';
+import { IValidator } from './validator';
+
+export class TypeValidation implements IValidator {
+  constructor(readonly value: any, readonly expectedType: string, readonly fieldName: string) {}
+
+  validate(): Error | undefined {
+    const fieldType = typeof this.value;
+    if (fieldType !== this.expectedType) {
+      return new IncorrectFieldTypeError(fieldType, this.expectedType, this.fieldName);
+    }
+  }
+}
